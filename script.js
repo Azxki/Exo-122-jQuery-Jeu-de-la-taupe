787571e4ba19d6f8280ebb192fb2d16347353394
azxki <cococoursan@gmail.com>
2022-12-20 13:59:13 +0100

    exo

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+let holes = $('.hole');
+let scoreBoard = $('.score');
+let moles = $('.mole');
+let button = $('#startGame');
+let departSecondes = 9
+let temps = departSecondes * 11
+let img = document.createElement('img');
+
+button.ready(function() {
+    $.startGame = function (){
+        let int = setInterval(() => {
+            //Création du compte à rebours 9 à 0 secondes
+            let secondes = parseInt(temps % 10, 10)
+            secondes = secondes < 10 ? + secondes : secondes
+            console.log("Secondes " + `${secondes}`);
+            temps = temps <= 9 ? 9 : temps - 1
+            //Selection random du trou
+            let hole = holes[Math.floor(Math.random()*holes.length)];
+            console.log(hole);
+            //Disparition des trous
+            hole = $('.hole').addClass('up').hide();
+            //Fin de partie quand le compte à rebours est à 0
+            if (secondes === 0) {
+                return clearInterval(int) & alert("La partie est terminée !");
+            }
+        }, 1000);
+    }
+    //Lancement de la fonction
+    button.click(function (){
+        $.startGame();
+    })
+})
+
